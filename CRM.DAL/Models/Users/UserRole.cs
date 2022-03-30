f1c541206d785daf6b8f4b53b12d9ff83d754fa6
demydko.yuri <yuridemydko@icloud.com>
2022-03-30 10:41:00 +0400

    fix: UserRoles

--- CRM.DAL/Models/Users/UserRole.cs
+++ CRM.DAL/Models/Users/UserRole.cs
@@ -9,11 +9,8 @@ namespace CRM.DAL.Models.Users
     {
         public User User { get; set; }
         
-        public string UserId { get; set; }
-
         public Role Role { get; set; }
         
-        public string RoleId { get; set; }
     }
     public class UserRoleConfiguration : IEntityTypeConfiguration<UserRole>
     {
