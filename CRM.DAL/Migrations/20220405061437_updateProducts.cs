5e0f050cdfc4ea5677613172e93433c70e286267
Ubuntu <ubuntu@ip-172-31-44-78.eu-central-1.compute.internal>
2022-04-05 06:16:43 +0000

    migration applied

---
+++ CRM.DAL/Migrations/20220405061437_updateProducts.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRM.DAL.Migrations
+{
+    public partial class updateProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AddedAt",
+                table: "Products",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Products",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Priority",
+                table: "Products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddedAt",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "Products");
+        }
+    }
+}
