db30ffcc7c57b2a3e4120be25825899e6a0e7af1
Ubuntu <ubuntu@ip-172-31-44-78.eu-central-1.compute.internal>
2022-04-16 08:31:16 +0000

    Migration

---
+++ CRM.DAL/Migrations/20220416082639_updatePayCards.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRM.DAL.Migrations
+{
+    public partial class updatePayCards : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ValidTill",
+                table: "PayCards",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(string),
+                oldType: "character varying(6)",
+                oldMaxLength: 6,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ValidTill",
+                table: "PayCards",
+                type: "character varying(6)",
+                maxLength: 6,
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+    }
+}
