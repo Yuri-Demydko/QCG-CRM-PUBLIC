fbf8382b610c5ee7e8e725fb99c50e9f20ef11d1
Ubuntu <ubuntu@ip-172-31-44-78.eu-central-1.compute.internal>
2022-04-18 17:02:42 +0000

    Migration

---
+++ CRM.DAL/Migrations/20220418160536_updateUsersRegDate_AddToken.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRM.DAL.Migrations
+{
+    public partial class updateUsersRegDate_AddToken : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RegistrationDate",
+                table: "AspNetUsers",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RegistrationDate",
+                table: "AspNetUsers");
+        }
+    }
+}
