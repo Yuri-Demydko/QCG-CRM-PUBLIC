471ede2303cd339e6564e8c5f5590bc5958b9c2c
Ubuntu <ubuntu@ip-172-31-44-78.eu-central-1.compute.internal>
2022-05-09 15:48:40 +0000

    Migration

--- CRM.DAL/Migrations/20220509154214_addSiaBalance.Designer.cs
+++ CRM.DAL/Migrations/20220509154214_addSiaBalance.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using CRM.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace CRM.DAL.Migrations
 {
     [DbContext(typeof(GeneralDbContext))]
-    partial class GeneralDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220509154214_addSiaBalance")]
+    partial class addSiaBalance
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
